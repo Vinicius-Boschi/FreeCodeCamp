cf19cbd8e91e598dc7528add43131f73a27bbbbb
Vinicius Boschi <vinicius.yg@gmail.com>
2022-06-07 19:49:55 -0300

    Adicionando projeto de cardápio

---
+++ Menu/assets/js/menu.js
@@ -0,0 +1,52 @@
+const menu = [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'assets/img/',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto, placeat, sint doloremque deleniti autem fugiat numquam nemo porro quaerat omnis dolore a ex saepe, aliquam mollitia! Tenetur, veniam incidunt.'
+    },
+    {
+        id: 2,
+        title: 'diner double',
+        category: 'lunch',
+        price: 13.99,
+        img: 'assets/img/',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto, placeat, sint doloremque deleniti autem fugiat numquam nemo porro quaerat omnis dolore a ex saepe, aliquam mollitia! Tenetur, veniam incidunt.'
+    },
+    {
+        id: 3,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 6.99,
+        img: 'assets/img/',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto, placeat, sint doloremque deleniti autem fugiat numquam nemo porro quaerat omnis dolore a ex saepe, aliquam mollitia! Tenetur, veniam incidunt.'
+    },
+    {
+        id: 4,
+        title: 'country delight',
+        category: 'lunch',
+        price: 20.99,
+        img: 'assets/img/',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto, placeat, sint doloremque deleniti autem fugiat numquam nemo porro quaerat omnis dolore a ex saepe, aliquam mollitia! Tenetur, veniam incidunt.'
+    },
+    {
+        id: 5,
+        title: 'egg atack',
+        category: 'lunch',
+        price: 22.99,
+        img: 'assets/img/',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto, placeat, sint doloremque deleniti autem fugiat numquam nemo porro quaerat omnis dolore a ex saepe, aliquam mollitia! Tenetur, veniam incidunt.'
+    },
+    {
+        id: 6,
+        title: 'oreo drum',
+        category: 'shakes',
+        price: 18.99,
+        img: 'assets/img/',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto, placeat, sint doloremque deleniti autem fugiat numquam nemo porro quaerat omnis dolore a ex saepe, aliquam mollitia! Tenetur, veniam incidunt.'
+    }
+]
+
+export { menu }
